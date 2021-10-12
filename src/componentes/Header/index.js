3c9b72db74acce7f768e756d5b589470ea92011e
Victor Santos <71085593+victorwsantos@users.noreply.github.com>
2021-10-12 02:42:22 -0300

    Estrutura
    Esturtura basica front end

---
+++ src/componentes/Header/index.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+export default function Header(){
+    return(
+        <div className='header-container'>
+            <div className='logo-area'>
+                <h2>Sua logo aqui</h2>
+            </div>
+            <div className='navigation-header'>
+                <ul>
+                    <li>Home</li>
+                    <li>Home</li>
+                    <li>Home</li>
+                    <li>Home</li>
+                </ul>
+            </div>
+        </div>
+    )
+}
